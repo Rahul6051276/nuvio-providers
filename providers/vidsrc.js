5e22eed206b711368e6b72ed3f3cc000e9bb049a
Rahul6051276 <techsmart397@gmail.com>
2026-03-13 16:06:32 +0530

    Update vidsrc.js

--- providers/vidsrc.js
+++ providers/vidsrc.js
@@ -1,34 +1,16 @@
-// Vidsrc Scraper for Nuvio - Hindi & Multi-Audio Version
-const name = "Pikashow Multi-Audio";
+const name = "Pikashow Server (Vidsrc)";
 const id = "vidsrc_pika";
 
 async function getStreams(tmdbId, mediaType = 'movie', seasonNum = null, episodeNum = null) {
-    // हम 2 अलग सर्वर्स का उपयोग करेंगे ताकि हिंदी मिलने के चांस बढ़ जाएँ
-    const servers = [
-        {
-            name: "Pikashow Server 1 (Multi)",
-            url: mediaType === 'movie' 
-                ? `https://vidsrc.xyz/embed/movie/${tmdbId}` 
-                : `https://vidsrc.xyz/embed/tv/${tmdbId}/${seasonNum}/${episodeNum}`
-        },
-        {
-            name: "Pikashow Server 2 (Hindi)",
-            url: mediaType === 'movie' 
-                ? `https://vidsrc.to/embed/movie/${tmdbId}` 
-                : `https://vidsrc.to/embed/tv/${tmdbId}/${seasonNum}/${episodeNum}`
-        }
-    ];
+    const url = mediaType === 'movie' 
+        ? `https://vidsrc.me/embed/movie?tmdb=${tmdbId}` 
+        : `https://vidsrc.me/embed/tv?tmdb=${tmdbId}&sea=${seasonNum}&epi=${episodeNum}`;
 
-    return servers.map(s => ({
-        name: s.name,
-        title: "Check Audio Settings for Hindi",
-        url: s.url,
-        type: "embed",
-        headers: {
-            'Referer': 'https://vidsrc.xyz/',
-            'User-Agent': 'Mozilla/5.0'
-        }
-    }));
+    return [{
+        name: "Pikashow Multi-Audio",
+        url: url,
+        type: "embed"
+    }];
 }
 
 if (typeof module !== 'undefined' && module.exports) {
